92ce375d7ad17e3da6bcfbc3ee0691270a769b92
Ivan Kanakarakis <ivan.kanak@gmail.com>
2011-05-02 14:21:06 +0300

    comic.js: code reuse

--- scripts/comic.js
+++ scripts/comic.js
@@ -1,15 +1,12 @@
 var lines;
 $(document).ready(function() {
   $.get('/files/comic.list', function(data) {
-    lines = data.split('\n');
-    var imgurl = lines[Math.floor(Math.random() * lines.length)].split('\t');
-    var imgttl = imgurl[1]; imgurl = imgurl[0];
-    $("#comic").prepend($(document.createElement("img")).attr({ src: imgurl, alt: 'comic', title: imgttl}));
+    lines = data.split('\n'); clickComic();
   });
 });
 function clickComic() {
   var imgurl = lines[Math.floor(Math.random() * lines.length)].split('\t');
   var imgttl = imgurl[1]; imgurl = imgurl[0];
-  $("#comic img").attr({ src: imgurl, title: imgttl});
+  $("#comic").attr({ src: imgurl, title: imgttl});
 }
 // vim: nofoldenable ts=2 sw=2
