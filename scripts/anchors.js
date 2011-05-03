d4b893c3a3d0376ee54f3b51032e58681219aaf4
Ivan Kanakarakis <ivan.kanak@gmail.com>
2011-05-03 05:43:55 +0300

    introduce pilcrow as an anchor link to id

---
+++ scripts/anchors.js
@@ -0,0 +1,5 @@
+$(function() {
+    $("h2").each(function(idx) { 
+        $(this).append( $(document.createElement("a")).attr({class:'anchor', href:'#'+this.getAttribute('id')}).text("¶"));
+    });
+});
