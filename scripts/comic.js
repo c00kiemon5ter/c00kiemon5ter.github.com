baa1ea77bd21bf1a63651300c860d286c4144b3e
Ivan Kanakarakis <ivan.kanak@gmail.com>
2011-05-06 15:03:37 +0300

    comic.js: correctly handle clicks

--- scripts/comic.js
+++ scripts/comic.js
@@ -2,20 +2,18 @@ var lines;
 $(function() {
   $.get('/files/comic.list', function(data) {
     lines = data.split("\n"); clickComic();
-    $('#comic').click(clickComic);
   });
 });
 function clickComic() {
-  changeImg('/images/load.gif', 'loading');
-  var line = lines[Math.floor(Math.random()*lines.length)].split("\t");
-  var url = line.shift(), src = line.shift();
-  $(document.createElement("img")).attr({src: src}).load(function() {
-    $(".meta a").attr({href: url});
-    changeImg(src, line);
-  });
-}
-function changeImg(source, title) {
-  $("#comic").fadeOut('fast', function() {
-    $("#comic").attr({src: source, title: title}).fadeIn();
+  $("#comic").unbind().fadeOut('fast', function() {
+    $("#comic").attr({src: '/images/load.gif', title: 'loading'}).fadeIn();
+    var line = lines[Math.floor(Math.random()*lines.length)].split("\t");
+    var url = line.shift(), src = line.shift();
+    $(document.createElement("img")).attr({src: src}).load(function() {
+      $(".meta a").attr({href: url});
+      $("#comic").attr({src: src, title: line}).fadeIn(function() {
+        $("#comic").click(clickComic);
+      });
+    });
   });
 }
